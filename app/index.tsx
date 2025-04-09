f75b095b1ba0d75c950729840116e1612b5270d1
Arya <jackrack040@gmail.com>
2025-04-09 15:05:00 -0400

    rnfes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 
-const RootLayout = () => {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text>RootLayout</Text>
+      <Text>Aora!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
-
-export default RootLayout;
+}
 
 const styles = StyleSheet.create({
   container: {
